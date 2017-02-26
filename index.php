ce0f10725fb2a9a91446136e8e183628b386bf18
Mehmet Can <mcanmutaf@outlook.com>
2017-02-26 05:02:29 +0300

    sorted

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+echo date('H-i');
+if (date('H') > 19 && date('H') < 23)
+{
+echo "string";
+
+}
+$eray[0]="nane";
+
+$file = fopen("data.txt",'r');
+$fil=("data.txt");
+
+$lines = file($fil, FILE_IGNORE_NEW_LINES);
+
+fclose($file);
+
+//echo $lines[2];
+  $nuevo="";
+$r=Count($lines);
+for ($i=0; $i <$r; $i++) {
+  # code...
+
+$nuevo= $nuevo.$lines[$i].",";
+}
+echo "<br>";
+//echo $nuevo;
+
+$elma = explode(",", $nuevo);
+
+//echo $elma[4];
+$reve[0]="";
+for ($i=0; $i <Count($elma)-1 ; $i++) {
+  # code...
+  $reve[$elma[$i+1]]=$elma[$i];
+  $i++;
+}
+
+//echo $reve[25];
+
+for ($i=1500; $i >0 ; $i--) {
+  # code...
+if (isset($reve[$i])==1) {
+
+ echo $reve[$i]." ".$i;echo "<br>";
+}
+}
+
+?>
